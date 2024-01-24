b3489db25bf72ca183020cf2dffe78f2e1d66818
Taylor Smith <taylor.smith@invisible.email>
2024-01-24 15:05:59 -0800

    linkedin login form info and iframe selector

--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
 
+const puppeteer = require("puppeteer-extra")
+const proxyChain = require("proxy-chain")
+const StealthPlugin = require("puppeteer-extra-plugin-stealth")
+const UserAgent = require("user-agents")
+
+puppeteer.use(StealthPlugin())
+
+const linkedin = "https://www.linkedin.com/campaignmanager/"
+const iframeSelector = "iframe[title='Login']"
+
+const generateUserAgent = new UserAgent({ deviceCategory: "desktop" })
+
+async function run() {
+  const oldProxyUrl = `http://${process.env.PROXY_USER}:${process.env.PROXY_PASS}_country-UnitedStates@24.199.75.16:31112`
+
+  const newProxyUrl = await proxyChain.anonymizeProxy(oldProxyUrl)
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [`--proxy-server=${newProxyUrl}`],
+  })
+  try {
+    const page = await browser.newPage()
+    await page.setUserAgent(generateUserAgent().toString())
+    await page.goto(linkedin)
+    await page.waitForSelector("iframe")
+    const iframeElementHandle = await page.$(iframeSelector)
+    const frame = await iframeElementHandle.contentFrame()
+    console.log(frame)
+    await page.screenshot({ path: "login.png", fullPage: true })
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+    await frame.type("#username", "TestUserName", { delay: 80 })
+    await frame.type("#password", "Test2Password", { delay: 75 })
+    await page.screenshot({ path: "example.png", fullPage: true })
+  } catch (error) {
+    console.error("Jobs scrape failed", error)
+  } finally {
+    await browser.close()
+    proxyChain.closeAnonymizedProxy(newProxyUrl, true)
+  }
+}
+
+run()
